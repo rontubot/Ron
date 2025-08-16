e092a777ed9d89ed31056be664487b647e73e0c2
rontubot <rontubot@gmail.com>
2025-08-16 16:18:55 +0000

    act

--- api.py
+++ api.py
@@ -27,11 +27,11 @@ app = FastAPI()
 # Configurar CORS para React  
 app.add_middleware(  
     CORSMiddleware,  
-    allow_origins=["*"],  # Temporalmente para debugging  
+    allow_origins=["http://localhost:3000", "https://redesigned-potato-v67vjjrggwq7fpg7-3000.app.github.dev/login"],  
     allow_credentials=True,  
     allow_methods=["*"],  
     allow_headers=["*"],  
-)
+)  
   
 security = HTTPBearer()  
   
