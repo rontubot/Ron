5455c4a889a92343b47db22f84568ff0cbe3292b
rontubot <rontubot@gmail.com>
2025-10-13 16:00:38 -0300

    act

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends, Header, Response      
+from fastapi import FastAPI, HTTPException, Depends, Header, Response, Request  
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials      
 from fastapi.middleware.cors import CORSMiddleware      
 from pydantic import BaseModel      
